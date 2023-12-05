741fb2b0a5965fe6a0080141846b10946d07b2f3
jordangm94 <jordan.guerrero.martinez@gmail.com>
2023-12-05 08:39:42 -0500

    feat: Implemented validation pipes at the global level, so that whenever we run validation decorator, validation pipe is run

--- nestjs-task-management/src/main.ts
+++ nestjs-task-management/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe()); //global pipe, will be applied to every route handler, //whenever you run validation decorator, run validation pipe
   await app.listen(3000);
 }
 bootstrap();
